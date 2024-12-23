a117d86bdd780a7d2fd9dd6c1dbb9cc8d5346ae6
joeyhiew <joeyhiew@live.com.sg>
2024-12-23 18:00:04 +0800

    feat: alignment

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,21 +18,21 @@ const About = () => {
           },
         ]}
       />
-      <main className="text-primary-text-dark">
+      <main className="text-primary-text-dark mt-12">
         <h1 className="text-3xl">
           I'm Joey, an engineer who's passionate about creating beautiful and user friendly applications.
         </h1>
 
-        <article className="mt-4">
+        <article className="mt-6 text-lg">
           During my University, I took several internships in companies like Perx technologies (startup), GovTech
           Singapore (government tech agency), and NCS (MNC). After graduation, I joined TikTok as a Frontend developer.
         </article>
-        <article className="mt-4">
+        <article className="mt-6 text-lg">
           I enjoy building websites and applications using the MERN stack with a focus in frontend.
         </article>
-        <article className="mt-4">
+        <article className="mt-6">
           <p className="text-2xl">Education</p>
-          <p>
+          <p className="text-lg mt-2">
             I graduated from Nanyang Technological University with a Bachelors in Engineering Computer Science (Highest
             Distinction)
           </p>
