e444a7c41923d84897e0633fa145d17fc09bdcaf
joeyhiew <joeyhiew@live.com.sg>
2024-12-23 16:03:06 +0800

    feat: seo

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -24,8 +24,6 @@ import summiz from './summiz.svg';
 import threads from './threads.svg';
 import arrow from './arrow.svg';
 import contact from './contact.svg';
-import soundon from './soundon.png';
-import soundoff from './soundoff.png';
 import mail from './mail.svg';
 
 export {
@@ -55,7 +53,5 @@ export {
   threads,
   arrow,
   contact,
-  soundon,
-  soundoff,
   mail,
 };
