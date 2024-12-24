2ddd3f1b58a8f05a0b5f7896a8ec37aecaa5efde
joeyhiew <joeyhiew@live.com.sg>
2024-12-24 14:00:13 +0800

    feat: center guide

--- src/components/Userguide.jsx
+++ src/components/Userguide.jsx
@@ -2,7 +2,7 @@ import { draghold } from '../assets/icons/index.js';
 
 const UserGuide = () => {
   return (
-    <div className="absolute top-28 ml-[50%] z-10 flex flex-col items-center">
+    <div className="absolute top-28 left-[50%] translate-x-[-50%] z-10 flex flex-col items-center">
       <img src={draghold} className="w-16" />
       <p>Drag to explore</p>
     </div>
