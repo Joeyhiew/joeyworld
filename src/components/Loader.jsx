8deeba59439d05b99663e91c37d620e55c19f463
joeyhiew <joeyhiew@live.com.sg>
2024-12-25 22:04:37 +0800

    feat: fix loader alignment

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -13,7 +13,7 @@ const Loader = ({ onUnmount, onMount }) => {
   }, []);
 
   return (
-    <Html className="left-[-22vw] flex flex-col items-center">
+    <Html className="left-[-25vw] flex flex-col items-center">
       <span className="text-dark-pink-500 text-3xl font-amatic">Joey's world loading...</span>
       <div className="h-[25px] w-[50vw] rounded-full  border-2 border-opacity-30 border-dark-pink-500">
         <div className="h-[21px] bg-red-300 rounded-full" style={{ width: `${progress}%` }} />
